f20ee1f4e5c415439b1f9a811c7879d5742d70d4
Jorge Barcena Lumbreras <j.barcenalumbreras@gmail.com>
2019-11-06 21:41:48 +0100

    Base project

---
+++ code/main.cpp
@@ -0,0 +1,55 @@
+// File: main.cpp
+// Description : Proyecto base con la libreria de SFML
+// Author: Jorge Bárcena Lumbreras
+
+// © Copyright (C) 2019  Jorge Bárcena Lumbreras
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+
+#include<SFML/Window.hpp>
+#include<SFML/Graphics.hpp>
+
+
+using namespace sf;
+
+int main()
+{
+	RenderWindow windows(VideoMode(800,600), "Borosa");
+	
+	bool exit = false;
+
+	do
+	{
+		Event event;
+
+		while (windows.pollEvent(event))
+		{
+			switch (event.type)
+			{
+				case Event::Closed:
+				{
+					exit = true;
+					break;
+				}
+			}
+			
+			windows.display();
+		}
+
+	} while (!exit);
+
+	return 0;
+}
